ac49b08c19f9718e0ebc9b2a60165f92c2b6cfa5
Quintin Balsdon <qbalsdon@gmail.com>
2024-03-02 14:44:03 +0000

    Update talkback version

--- src/main/kotlin/com/balsdon/androidallyplugin/constants.kt
+++ src/main/kotlin/com/balsdon/androidallyplugin/constants.kt
@@ -5,3 +5,4 @@ const val elementMaxHeight = 40 // TODO: This should be a calculation
 const val TB4DWebPage = "https://ally-keys.com/tb4d.html"
 const val TB4DInstallHelpWebPage = "$TB4DWebPage#installation-help"
 const val TB4DPackageName = "com.android.talkback4d"
+const val latestApkFileName = "talkback-phone-release-signed-73.apk"
