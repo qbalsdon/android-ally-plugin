be77da6e2b6abe39f0f3f29c7a3fd86947d0c6a3
Quintin Balsdon <qbalsdon@gmail.com>
2024-03-18 23:14:51 +0000

    Fixes for dark mode selection

--- src/main/kotlin/com/balsdon/androidallyplugin/ui/CustomIcon.kt
+++ src/main/kotlin/com/balsdon/androidallyplugin/ui/CustomIcon.kt
@@ -1,7 +1,6 @@
 package com.balsdon.androidallyplugin.ui
 
 import com.intellij.openapi.util.IconLoader
-import com.intellij.util.ui.UIUtil
 import javax.swing.Icon
 
 
@@ -10,43 +9,23 @@ enum class CustomIcon(private val fileReference: String) {
     EMULATOR("emulator"),
     A11Y_OPEN_MENU("open_menu"),
     REFRESH("sync"),
-    A11Y_EXPAND_MORE("expand_more"),
     A11Y_ACTIONS("actions"),
-    A11Y_CONTROLS("controls"),
-    A11Y_MORE_MENU("more_option"),
     A11Y_SWIPE_LEFT("swipe_left"),
     A11Y_SWIPE_RIGHT("swipe_right"),
     A11Y_TAP("tap"),
     A11Y_TAP_LONG("tap_long"),
-    TOGGLE_TALKBACK("accessibility"),
-    TOGGLE_FONT_SCALE("font_scale"),
-    TOGGLE_ANIMATIONS("animation"),
-    TOGGLE_DARK_MODE("dark_mode"),
-    COLOR_MODE("colour"),
-    LAUNCH_SCREEN("launch"),
-    SIZE_OPTIONS("size"),
-    HIGH_PRIORITY("priority_high"),
     INFO("info"),
-    TOGGLE_VOICE("voice"),
-    TOGGLE_TOAST("toast"),
     DEVICE_BACK("back"),
     DEVICE_HOME("home"),
-    DEVICE_POWER("power"),
-    SCROLL_TO_TOP("scroll_to_top"),
-    SCROLL_TO_BOTTOM("scroll_to_bottom"),
-    SCROLL_UP("scroll_up"),
-    SCROLL_DOWN("scroll_down"),
-    TALKBACK_TUTORIAL("tutorial")
     ;
 
+    private fun createFileReference() = "$imageFolder$fileReference$imageType"
     fun create(): Icon =
         IconLoader.getIcon(
-            "$imageFolder$fileReference${if (UIUtil.isUnderDarcula()) darkModeId else lightModeId}$imageType",
+            createFileReference(),
             CustomIcon::class.java
         )
 
     private val imageFolder = "/icons/"
     private val imageType = ".svg"
-    private val lightModeId = "_lm"
-    private val darkModeId = "_dm"
 }
