605d4107653c45bcdb7038746b70858a2d0dfd45
Quintin Balsdon <qbalsdon@gmail.com>
2024-10-21 16:02:20 +0100

    Added a hack download for the performanceTesting.jar

--- performanceTestingTempFix.sh
+++ performanceTestingTempFix.sh
@@ -2,10 +2,16 @@
 
 # https://github.com/JetBrains/intellij-platform-gradle-plugin/issues/1738
 
-FOLDER=/Users/$USER/.gradle/caches/transforms-3/0efa82f00626821fb6f42f42cc9d940c/transformed/android-studio-2024.2.1.10-mac_arm/plugins
-mkdir -p $FOLDER/performanceTesting
-mkdir -p $FOLDER/performanceTesting/lib
-FOLDER=$FOLDER/performanceTesting/lib/
+FOLDER=/Users/${USER}/.gradle/caches/transforms-3/0efa82f00626821fb6f42f42cc9d940c/transformed/android-studio-2024.2.1.10-mac_arm/plugins
+#FOLDER=/home/runner/.gradle/caches/transforms-3/e773a64bc76e36769f59d43df2c66fa6/transformed/android-studio-2024.2.1.10-linux/plugins/performanceTesting/lib/performanceTesting.jar
+mkdir -p ${FOLDER}/performanceTesting
+mkdir -p ${FOLDER}/performanceTesting/lib
 
-open ${FOLDER}
-open https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/performanceTesting/performance-testing/242.23339.19/performance-testing-242.23339.19.jar
+FOLDER=${FOLDER}/performanceTesting/lib
+FILE=performance-testing-242.23339.19.jar
+URL=https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/performanceTesting/performance-testing/242.23339.19/${FILE}
+
+echo ${URL}
+curl -I ${URL} -o ${FOLDER}/${FILE}
+
+#curl https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/performanceTesting/performance-testing/242.23339.19/performance-testing-242.23339.19.jar -o /Users/${USER}/.gradle/caches/transforms-3/0efa82f00626821fb6f42f42cc9d940c/transformed/android-studio-2024.2.1.10-mac_arm/plugins/performanceTesting/lib/performance-testing-242.23339.19.jar
